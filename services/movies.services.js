5c0762e31b09caffd374cb71a8557e77e1b517bf
Gopirg07 <gopinathofficial16@gmail.com>
2023-01-13 15:40:13 +0530

    id  to _id

--- services/movies.services.js
+++ services/movies.services.js
@@ -1,16 +1,17 @@
+import { ObjectId } from "mongodb";
 import { client } from "../index.js ";
 
 export async function updateMovieById(id, data) {
-    return await client.db("gopi").collection("gopi").updateOne({ id: id }, { $set: data });
+    return await client.db("gopi").collection("gopi").updateOne({ _id:ObjectId(id) }, { $set: data });
 }
 export async function deleteMovieById(id) {
-    return await client.db("gopi").collection("gopi").deleteOne({ id: id });
+    return await client.db("gopi").collection("gopi").deleteOne({ _id:ObjectId(id) });
 }
 export async function createMovies(data) {
     return await client.db("gopi").collection("gopi").insertMany(data);
 }
 export async function getMoviesById(id) {
-    return await client.db("gopi").collection("gopi").findOne({ id: id });
+    return await client.db("gopi").collection("gopi").findOne({ _id:ObjectId(id) });
 }
 export async function getMovies(request) {
     return await client.db("gopi").collection("gopi").find(request.query).toArray();
